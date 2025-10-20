1ff443527c167ec71649b185fc8623a4e40c1851
Fuchen Shi <fuchen@emq.com>
2025-10-20 20:53:28 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,7 @@ fn main() {
         2 => {
             let cmd = &args[1];
             match &cmd[..] {
-                "init" => {
-                    fs::create_dir(".git").unwrap();
-                    fs::create_dir(".git/objects").unwrap();
-                    fs::create_dir(".git/refs").unwrap();
-                    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-                    println!("Initialized git directory")
-                }
+                "init" => init(),
                 _ => println!("unknown command"),
             }
         }
@@ -30,51 +24,14 @@ fn main() {
                 "cat-file" => {
                     let flag = &args[2];
                     match &flag[..] {
-                        "-p" => {
-                            let object_hash = &args[3];
-                            let (hash_prefix, hash_suffix) = object_hash.split_at(2);
-                            let object_path = Path::new(".git/objects")
-                                .join(hash_prefix)
-                                .join(hash_suffix);
-
-                            let compressed_data = fs::read(object_path).unwrap();
-                            let mut decoder = ZlibDecoder::new(&compressed_data[..]);
-                            let mut decompressed_data = String::new();
-                            decoder.read_to_string(&mut decompressed_data).unwrap();
-
-                            let (_, object_content) = decompressed_data.split_once('\0').unwrap();
-                            print!("{}", object_content)
-                        }
+                        "-p" => read_blob(&args[3]),
                         _ => println!("unknown command"),
                     }
                 }
                 "hash-object" => {
                     let flag = &args[2];
                     match &flag[..] {
-                        "-w" => {
-                            let file_path = &args[3];
-                            let file_content = fs::read(file_path).unwrap();
-
-                            let object_header = format!("blob {}\0", file_content.len());
-                            let object_header_bytes = object_header.into_bytes();
-                            let object_data = [object_header_bytes, file_content].concat();
-
-                            let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
-                            encoder.write_all(&object_data).unwrap();
-                            let compressed_data = encoder.finish().unwrap();
-
-                            let mut hasher = Sha1::new();
-                            hasher.update(&object_data);
-                            let object_hash = hasher.finalize();
-                            let object_hash_string = format!("{:x}", object_hash);
-                            let (hash_prefix, hash_suffix) = object_hash_string.split_at(2);
-                            let object_dir = Path::new(".git/objects").join(hash_prefix);
-                            let object_path = object_dir.join(hash_suffix);
-
-                            fs::create_dir_all(object_dir).unwrap();
-                            fs::write(object_path, compressed_data).unwrap();
-                            println!("{}", object_hash_string)
-                        }
+                        "-w" => create_blob(&args[3]),
                         _ => println!("unknown command"),
                     }
                 }
@@ -83,4 +40,51 @@ fn main() {
         }
         _ => println!("unknown command"),
     }
+}
+
+fn init() {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+    println!("Initialized git directory")
+}
+
+fn read_blob(object_hash: &String) {
+    let (hash_prefix, hash_suffix) = object_hash.split_at(2);
+    let object_path = Path::new(".git/objects")
+        .join(hash_prefix)
+        .join(hash_suffix);
+
+    let compressed_data = fs::read(object_path).unwrap();
+    let mut decoder = ZlibDecoder::new(&compressed_data[..]);
+    let mut decompressed_data = String::new();
+    decoder.read_to_string(&mut decompressed_data).unwrap();
+
+    let (_, object_content) = decompressed_data.split_once('\0').unwrap();
+    print!("{}", object_content)
+}
+
+fn create_blob(file_path: &String) {
+    let file_content = fs::read(file_path).unwrap();
+
+    let object_header = format!("blob {}\0", file_content.len());
+    let object_header_bytes = object_header.into_bytes();
+    let object_data = [object_header_bytes, file_content].concat();
+
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(&object_data).unwrap();
+    let compressed_data = encoder.finish().unwrap();
+
+    let mut hasher = Sha1::new();
+    hasher.update(&object_data);
+    let object_hash = hasher.finalize();
+    let object_hash_string = format!("{:x}", object_hash);
+    let (hash_prefix, hash_suffix) = object_hash_string.split_at(2);
+    let object_dir = Path::new(".git/objects").join(hash_prefix);
+    let object_path = object_dir.join(hash_suffix);
+
+    fs::create_dir_all(object_dir).unwrap();
+    fs::write(object_path, compressed_data).unwrap();
+    println!("{}", object_hash_string)
 }
